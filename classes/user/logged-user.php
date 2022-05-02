5471ce2909b64fe5eeb5f4692687e28a8f32e96f
LaSe28 <mattia.las28@gmail.com>
2022-05-02 16:26:33 +0200

    end user

--- classes/user/logged-user.php
+++ classes/user/logged-user.php
@@ -4,14 +4,44 @@ include_once __DIR__ . '/user.php';
 class LoggedUser extends User
 {
   private $birth;
+  private $discount;
 
   public function __construct($_name ,$_surname, $_birth)
   {
     parent::__construct($_name, $_surname);
     $this->birth = $_birth;
   }
+
+  /**
+   * Get the value of birth
+   */ 
+  public function getBirth()
+  {
+    return $this->birth;
+  }
+
+  /**
+   * Get the value of discount
+   */ 
+  public function getDiscount()
+  {
+    return $this->discount;
+  }
+
+  /**
+   * Set the value of discount
+   *
+   * @return  self
+   */ 
+  public function setDiscount($discount)
+  {
+    $this->discount = $discount;
+
+    return $this;
+  }
 }
 
 $prova = new LoggedUser('mattia','la selva','28/03/1996');
+$prova->setDiscount(20);
 
 var_dump($prova);
